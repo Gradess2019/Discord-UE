15269645ae3435fcb279c701358a299072e32119
Gradess <trofimovjava@gmail.com>
2021-06-24 00:13:50 +0300

    Add UDiscordUpdateActivityAction

---
+++ Source/DiscordSDK/Private/Actions/DiscordUpdateActivityAction.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actions/DiscordUpdateActivityAction.h"
+
+#include "DiscordObject.h"
+#include "types.h"
+
+UDiscordUpdateActivityAction* UDiscordUpdateActivityAction::UpdateActivity(
+	const FString State,
+	const FString Details
+)
+{
+	UDiscordUpdateActivityAction* Node = NewObject<UDiscordUpdateActivityAction>();
+	Node->State = State;
+	Node->Details = Details;
+	return Node;
+}
+
+void UDiscordUpdateActivityAction::Activate()
+{
+	auto Core = UDiscordObject::GetCore();
+	if (!Core) { return; }
+
+	discord::Activity activity{};
+
+	const auto ConvertedState = StringCast<ANSICHAR>(*State).Get();
+	const auto ConvertedDetails = StringCast<ANSICHAR>(*Details).Get();
+	
+	activity.SetState(ConvertedState);
+	activity.SetDetails(ConvertedDetails);
+	
+	Core->ActivityManager().UpdateActivity(activity, [&](discord::Result result)
+	{
+		Finished.Broadcast();
+	});
+}
