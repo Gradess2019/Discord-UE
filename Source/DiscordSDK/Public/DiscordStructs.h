b2ab8d088cb661c44a713a14f29d00c75391bbb0
Stepan Trofimov <trofimovjava@gmail.com>
2021-06-25 17:06:00 +0300

    Add FDiscordActivityAssets

--- Source/DiscordSDK/Public/DiscordStructs.h
+++ Source/DiscordSDK/Public/DiscordStructs.h
@@ -6,6 +6,46 @@
 #include "DiscordStructs.generated.h"
 
 USTRUCT(BlueprintType)
+struct FDiscordActivityAssets
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Discord|Activity")
+	FString LargeImageHash;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Discord|Activity")
+	FString LargeText;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Discord|Activity")
+	FString SmallImageHash;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Discord|Activity")
+	FString SmallText;
+
+	FORCEINLINE discord::ActivityAssets* Create() const
+	{
+		const auto Assets = new discord::ActivityAssets();
+		Init(*Assets);
+
+		return Assets;
+	}
+
+	FORCEINLINE void Init(discord::ActivityAssets& Assets) const
+	{
+		Assets.SetLargeImage(TCHAR_TO_ANSI(*LargeImageHash));
+		Assets.SetLargeText(TCHAR_TO_ANSI(*LargeText));
+		Assets.SetSmallImage(TCHAR_TO_ANSI(*SmallImageHash));
+		Assets.SetSmallText(TCHAR_TO_ANSI(*SmallText));
+	}
+
+	FORCEINLINE void Init(discord::Activity* Activity) const
+	{
+		auto& Assets = Activity->GetAssets();
+		Init(Assets);
+	}
+};
+
+USTRUCT(BlueprintType)
 struct FDiscordActivity
 {
 	GENERATED_BODY()
@@ -22,6 +62,9 @@ struct FDiscordActivity
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Discord|Activity")
 	TEnumAsByte<EDiscordActivityType> Type;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Discord|Activity")
+	FDiscordActivityAssets Assets;
+
 	FORCEINLINE discord::Activity* Create() const
 	{
 		auto Activity = new discord::Activity();
@@ -29,6 +72,8 @@ struct FDiscordActivity
 		Activity->SetState(TCHAR_TO_ANSI(*State));
 		Activity->SetDetails(TCHAR_TO_ANSI(*Details));
 		Activity->SetType(static_cast<discord::ActivityType>(Type.GetValue()));
+
+		Assets.Init(Activity);
 		
 		return Activity;
 	}
