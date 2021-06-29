c0ddbbd48755d0ab64489b6420890ed0ee34f2ba
Stepan Trofimov <trofimovjava@gmail.com>
2021-06-30 01:06:03 +0300

    Expose activity events

--- Source/DiscordSDK/Private/DiscordObject.cpp
+++ Source/DiscordSDK/Private/DiscordObject.cpp
@@ -3,6 +3,8 @@
 
 #include "DiscordObject.h"
 
+#include "Wrappers/DiscordUser.h"
+
 discord::Core* UDiscordObject::Core = nullptr;
 
 void UDiscordObject::Init_Implementation(const FDiscordActivityManagerData& ActivityManager)
@@ -36,14 +38,46 @@ void UDiscordObject::InitUserManager()
 EDiscordActionResult UDiscordObject::InitActivityManager(const FDiscordActivityManagerData& ActivityManager)
 {
 	discord::Result Result = discord::Result::NotFound;
-	
+
 	if (!ActivityManager.Command.IsEmpty())
 	{
 		Result = Core->ActivityManager().RegisterCommand(TCHAR_TO_ANSI(*ActivityManager.Command));
-	} else if (ActivityManager.SteamAppId > 0)
+	}
+	else if (ActivityManager.SteamAppId > 0)
 	{
 		Result = Core->ActivityManager().RegisterSteam(ActivityManager.SteamAppId);
 	}
+
+	const auto OnActivityInviteCallback = std::bind(
+		&UDiscordObject::OnActivityInviteCallback,
+		this,
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3
+	);
+
+	const auto OnActivityJoinCallback = std::bind(
+		&UDiscordObject::OnActivityJoinCallback,
+		this,
+		std::placeholders::_1
+	);
+
+	const auto OnActivitySpectateCallback = std::bind(
+		&UDiscordObject::OnActivitySpectateCallback,
+		this,
+		std::placeholders::_1
+	);
+	
+	const auto OnActivityJoinRequestCallback = std::bind(
+		&UDiscordObject::OnActivityJoinRequestCallback,
+		this,
+		std::placeholders::_1
+	);
+
+	Core->ActivityManager().OnActivityInvite.Connect(OnActivityInviteCallback);
+	Core->ActivityManager().OnActivityJoin.Connect(OnActivityJoinCallback);
+	Core->ActivityManager().OnActivitySpectate.Connect(OnActivitySpectateCallback);
+	Core->ActivityManager().OnActivityJoinRequest.Connect(OnActivityJoinRequestCallback);
 
 	return static_cast<EDiscordActionResult>(Result);
 }
@@ -73,4 +107,42 @@ void UDiscordObject::Update_Implementation()
 {
 	if (!Core) { return; }
 	Core->RunCallbacks();
+}
+
+void UDiscordObject::OnActivityInviteCallback(
+	discord::ActivityActionType Type,
+	discord::User const& User,
+	discord::Activity const& Activity
+)
+{
+	const auto DiscordType = static_cast<EDiscordActivityActionType>(Type);
+
+	auto DiscordUser = NewObject<UDiscordUser>();
+	DiscordUser->Init(&User);
+
+	OnActivityInvite.Broadcast(DiscordType, DiscordUser, FDiscordActivity());
+}
+
+void UDiscordObject::OnActivityJoinCallback(
+	char const* JoinSecret
+)
+{
+	OnActivityJoin.Broadcast(FString(JoinSecret));
+}
+
+void UDiscordObject::OnActivitySpectateCallback(
+	char const* SpectateSecret
+)
+{
+	OnActivitySpectate.Broadcast(FString(SpectateSecret));
+}
+
+void UDiscordObject::OnActivityJoinRequestCallback(
+	discord::User const& User
+)
+{
+	auto DiscordUser = NewObject<UDiscordUser>();
+	DiscordUser->Init(&User);
+	
+	OnActivityJoinRequest.Broadcast(DiscordUser);
 }
--- Source/DiscordSDK/Public/DiscordObject.h
+++ Source/DiscordSDK/Public/DiscordObject.h
@@ -3,9 +3,17 @@
 #pragma once
 #include "CoreMinimal.h"
 
+#include "DiscordEnums.h"
 #include "DiscordStructs.h"
 #include "DiscordSDK/Native/core.h"
 #include "DiscordObject.generated.h"
+
+class UDiscordUser;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FActivityInvite, EDiscordActivityActionType, Type, UDiscordUser*, User, FDiscordActivity, Activity);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActivityJoin, FString, JoinSecret);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActivitySpectate, FString, SpectateSecret);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActivityJoinRequest, UDiscordUser*, User);
 
 /**
  * 
@@ -21,23 +29,8 @@ public:
 		const FDiscordActivityManagerData& ActivityManager
 	);
 
-protected:
-	UFUNCTION()
-	void InitCore();
-	
-	UFUNCTION()
-	void InitUserManager();
-	
-	UFUNCTION(BlueprintCallable, Category = "Discord|Core")
-	EDiscordActionResult InitActivityManager(const FDiscordActivityManagerData& ActivityManager);
-
-	UFUNCTION()
-	void InitTimer(UWorld* World);
-	
-public:
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Discord|Core")
 	void Update();
-
 
 	// TODO: How can we avoid Singleton pattern here?
 	static discord::Core* GetCore();
@@ -45,6 +38,51 @@ public:
 	virtual void BeginDestroy() override;
 
 protected:
+	UFUNCTION()
+	void InitCore();
+
+	UFUNCTION()
+	void InitUserManager();
+
+	UFUNCTION(BlueprintCallable, Category = "Discord|Core")
+	EDiscordActionResult InitActivityManager(const FDiscordActivityManagerData& ActivityManager);
+
+	UFUNCTION()
+	void InitTimer(UWorld* World);
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Discord|Core")
+	FActivityInvite OnActivityInvite;
+	
+	UPROPERTY(BlueprintAssignable, Category = "Discord|Core")
+	FActivityJoin OnActivityJoin;
+	
+	UPROPERTY(BlueprintAssignable, Category = "Discord|Core")
+	FActivitySpectate OnActivitySpectate;
+	
+	UPROPERTY(BlueprintAssignable, Category = "Discord|Core")
+	FActivityJoinRequest OnActivityJoinRequest;
+	
+protected:
+	void OnActivityInviteCallback(
+		discord::ActivityActionType Type,
+		discord::User const& User,
+		discord::Activity const& Activity
+	);
+
+	void OnActivityJoinCallback(
+		char const* JoinSecret
+	);
+	
+	void OnActivitySpectateCallback(
+		char const* SpectateSecret
+	);
+	
+	void OnActivityJoinRequestCallback(
+		discord::User const& User
+	);
+	
+
 	UPROPERTY(BlueprintReadWrite, Category = "Discord|Core")
 	FTimerHandle UpdateTimer;
 
